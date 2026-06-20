317b055062a4eedf69112573c19df7ddb9317210
Ulaaka <133600932+Ulaaka@users.noreply.github.com>
2026-06-20 01:32:49 +0100

    added more queries

--- db_queries.py
+++ db_queries.py
@@ -22,3 +22,17 @@ class QueryProcessor:
         sql = """INSERT IGNORE INTO watch_history (video_url, video_name, channel_url, channel_name, time_stamp) VALUES (%s,%s,%s,%s,%s)"""
         self.cursor.executemany(sql, history_list)
         self.db.commit()
+
+    def the_most_watched_channel(self):
+
+        sql = "SELECT channel_name, channel_url, COUNT(*) AS count FROM watch_history GROUP BY channel_name, channel_url ORDER BY count DESC LIMIT 20"
+        self.cursor.execute(sql)
+        output = self.cursor.fetchall()
+        return output if output else None
+
+    def the_most_watched_videos(self):
+
+        sql = "SELECT video_name, video_url, COUNT(*) AS count FROM watch_history GROUP BY video_name, video_url ORDER BY count DESC LIMIT 20"
+        self.cursor.execute(sql)
+        output = self.cursor.fetchall()
+        return output if output else None
--- test_file.py
+++ test_file.py
@@ -1,8 +1,12 @@
 from html_parser import HTML_Parser
 from html_processor import HTML_Processor
+from db_queries import QueryProcessor
 import os
 
 path = os.path.join("data_folder", "watch-history.html")
 
-parser = HTML_Parser(path)
-processor = HTML_Processor(parser.history_list)
+#parser = HTML_Parser(path)
+#processor = HTML_Processor(parser.history_list)
+
+query = QueryProcessor()
+print(query.the_most_watched_videos())
